fbc8d30a07a76017e5c5cfba6f289098aa14c2e2
Volodymyr Zahorodnyy <vovazahorodniy@gmail.com>
2021-01-25 11:05:13 -0800

    final deploy zeet

--- server/index.js
+++ server/index.js
@@ -16,8 +16,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use('/user', userRouter);  
 
-
-const CONNECTION_URL = 'mongodb+srv://BlogMaster:blog1995master@cluster0.ovjpb.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb://root:springfield1995@blogmongo-production/blog';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
